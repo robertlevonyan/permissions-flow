1a07f322454ab4c6c1f82bb1813413b9d1956d15
robertlevonyan <robert.levonyan@picsart.com>
2020-06-05 20:25:49 +0400

    new version

--- lib/src/main/java/com/innfinity/permissionflow/lib/PermissionFragment.kt
+++ lib/src/main/java/com/innfinity/permissionflow/lib/PermissionFragment.kt
@@ -59,6 +59,7 @@ class PermissionFragment : Fragment() {
         if (completableDeferred.isActive) {
             completableDeferred.cancel()
         }
+        completableDeferred = CompletableDeferred()
     }
 
 }
